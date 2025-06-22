58841be4a1cb31058f8dac1bf0e1b25f9547129d
Yousef M. Eldaly <94533646+YousefEldaly@users.noreply.github.com>
2025-06-23 01:42:38 +0300

    Update Evaluations.py solved name errors

--- Evaluations.py
+++ Evaluations.py
@@ -1,14 +1,15 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, cohen_kappa_score, f1_score
 
 class Evaluations():
-    def __init__ (self,
-                   history,
-                   y_pred,
-                   y_true,
-                   loss_score,
-                   error,
-                   validation_score):
+    def __init__(self,
+                 history,
+                 y_pred,
+                 y_true,
+                 loss_score,
+                 error,
+                 validation_score):
         self.history = history
         self.y_pred = y_pred
         self.y_true = y_true
@@ -16,61 +17,51 @@ class Evaluations():
         self.error = error
         self.validation_score = validation_score
         
-        self.matrix,self.kappa = self.matrix_and_kappa(self.y_pred,self.y_true)
-        self.f1 = self.f1_score(self.y_pred,self.y_true)
+        self.matrix, self.kappa = self.matrix_and_kappa(self.y_pred, self.y_true)
+        self.f1 = self.compute_f1_score(self.y_pred, self.y_true)  # Renamed to avoid conflict
         
     # Matrix confusion and the kappa value
-    def matrix_and_kappa(self,y_pred,y_true):
+    def matrix_and_kappa(self, y_pred, y_true):
+        # Convert probabilities to class labels
+        y_pred_labels = []
         for i in y_pred:
-            if i[0]>i[1]:
-                i[0]=1
-                i[1]=0
+            if i[0] > i[1]:
+                y_pred_labels.append(0)
             else:
-                i[1]=1
-                i[0]=0
-        y_pred = np.array(y_pred)
-        y_pred_1 = []
-        y_true_1 = []
-        for i in y_pred:
-            if i[0]==1:
-                y_pred_1.append(1)
-            else:
-                y_pred_1.append(2)
+                y_pred_labels.append(1)
+        
+        y_true_labels = []
         for i in y_true:
-            if i[0]==1:
-                y_true_1.append(1)
+            if i[0] > i[1]:
+                y_true_labels.append(0)
             else:
-                y_true_1.append(2)
-        C2 = confusion_matrix(y_true_1,y_pred_1)
-        kappa_value = cohen_kappa_score(y_true_1, y_pred_1)
-        return C2,kappa_value
+                y_true_labels.append(1)
+        
+        C2 = confusion_matrix(y_true_labels, y_pred_labels)
+        kappa_value = cohen_kappa_score(y_true_labels, y_pred_labels)
+        return C2, kappa_value
     
-    # f1
-    def f1_score(self,y_pred,y_true):
+    # f1 score calculation (renamed to avoid conflict with sklearn's f1_score)
+    def compute_f1_score(self, y_pred, y_true):
+        # Convert probabilities to class labels
+        y_pred_labels = []
         for i in y_pred:
-            if i[0]>i[1]:
-                i[0]=1
-                i[1]=0
+            if i[0] > i[1]:
+                y_pred_labels.append(0)
             else:
-                i[1]=1
-                i[0]=0
-        y_pred = np.array(y_pred)
-        y_pred_1 = []
-        y_true_1 = []
-        for i in y_pred:
-            if i[0]==1:
-                y_pred_1.append(1)
-            else:
-                y_pred_1.append(2)
+                y_pred_labels.append(1)
+        
+        y_true_labels = []
         for i in y_true:
-            if i[0]==1:
-                y_true_1.append(1)
+            if i[0] > i[1]:
+                y_true_labels.append(0)
             else:
-                y_true_1.append(2)
-        f1 = f1_score(y_true,y_pred, average = None)
+                y_true_labels.append(1)
+        
+        f1 = f1_score(y_true_labels, y_pred_labels, average=None)
         return f1
     
-    def draw_pict(self,history,types = 1):
+    def draw_pict(self, history, types=1):
         if types == 1:
             plt.plot(history.history['binary_accuracy'])
             plt.plot(history.history['val_binary_accuracy'])
@@ -79,6 +70,7 @@ class Evaluations():
             plt.legend(('train', 'validation'), loc='lower right')  
             plt.title('accuracy')
             plt.show()
+            
             plt.plot(history.history['loss'])
             plt.plot(history.history['val_loss'])
             plt.ylabel('loss') 
@@ -94,6 +86,7 @@ class Evaluations():
             plt.legend(('train', 'validation'), loc='lower right')  
             plt.title('accuracy')
             plt.show()
+            
             plt.plot(history.history['loss'])
             plt.plot(history.history['val_loss'])
             plt.ylabel('loss') 
@@ -101,4 +94,3 @@ class Evaluations():
             plt.legend(('train', 'validation'), loc='upper right')  
             plt.title('loss')
             plt.show()
-        return
